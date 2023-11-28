6cc259a949e19e16a4c6a522dc10d4af831ee904
denydemang <denyk819@gmail.com>
2023-11-28 14:06:14 +0700

    memodifikasi response api unit

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -12,17 +12,32 @@ class UnitController extends Controller
     public function getall() :JsonResponse{
         $dataunit =UnitView::all();
 
-        return response()->json($dataunit)->setStatusCode(200);
+        return response()->json(
+            [
+                'data' =>  $dataunit,
+                'success' => 'Successfully Get All Unit'
+
+            ])->setStatusCode(200);
     }
     public function getgroup($id_unit) :JsonResponse{
 
         $groupname = UnitView::where('id_unit', $id_unit)->first(['group_name'])->group_name;
 
         $dataunit = UnitView::where('group_name', $groupname)->get();
-        return response()->json($dataunit)->setStatusCode(200);
+        return response()->json([
+            [
+                'data' =>  $dataunit,
+                'success' => 'Successfully Get Group Unit'
+
+            ]
+        ])->setStatusCode(200);
     }
     public function getdefault() : JsonResponse{
         $dataunit = UnitView::where('convert_value', 1)->get();
-        return response()->json($dataunit)->setStatusCode(200);
+        return response()->json(
+            [
+                'data' => $dataunit,
+                'success' => 'Successfully Get Unit Default'
+            ])->setStatusCode(200);
     }
 }
